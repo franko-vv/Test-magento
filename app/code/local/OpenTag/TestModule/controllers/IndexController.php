f733e569b8e94ab4ab15ed623f7725d4506f1258
f1tzgerald <alter.vetal@mail.ru>
2016-06-21 22:28:30 +0300

    add view in controller

--- app/code/local/OpenTag/TestModule/controllers/IndexController.php
+++ app/code/local/OpenTag/TestModule/controllers/IndexController.php
@@ -5,6 +5,24 @@ class OpenTag_TestModule_IndexController extends Mage_Core_Controller_Front_Acti
     public function indexAction()
     {
         $this->loadLayout();
+
+        // Add new block - rwd/template/testmodule/contact.phtml
+        $block = $this->getLayout()->createBlock(
+            'Mage_Core_Block_Template',
+            'contact',
+            array(
+                'template' => 'testmodule/contact.phtml'
+            )
+        );
+
+        $this->getLayout()->getBlock('content')->append($block);
+
         $this->renderLayout();
     }
+
+    public function saveData()
+    {
+        echo "Data saved";
+    }
+
 }
