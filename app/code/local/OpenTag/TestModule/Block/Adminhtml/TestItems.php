8bca1ee1de2486bf85b85eeae5b1eee8df6d62d7
f1tzgerald <alter.vetal@mail.ru>
2016-06-24 11:53:18 +0300

    fix grid container add new item

--- app/code/local/OpenTag/TestModule/Block/Adminhtml/TestItems.php
+++ app/code/local/OpenTag/TestModule/Block/Adminhtml/TestItems.php
@@ -1,21 +1,24 @@
 <?php
-class OpenTag_TestModule_Block_AdminHtml_TestItems
-    extends Mage_Adminhtml_Block_Widget_Grid_Container
+class OpenTag_TestModule_Block_AdminHtml_TestItems extends Mage_Adminhtml_Block_Widget_Grid_Container
 {
+    /**
+     * Config Grid Container
+     */
     protected function _construct()
     {
-        //parent::_construct();
-
         $this->_addButtonLabel = Mage::helper('opentag_testmodule')->__('Add new item');
 
         $this->_blockGroup = 'opentag_testmodule';      // имя модуля
         $this->_controller = 'adminhtml_testitems';     // путь относительно корня модуля где лежит grid
 
-        $this->_headerText = Mage::helper('opentag_testmodule')->__('Test Items');
+        $this->_headerText = Mage::helper('opentag_testmodule')->__('Admin - CRUD fot Test Items');
     }
 
+    /**
+     * @return Set url for button Add new Item
+     */
     public function getCreateUrl()
     {
-        return $this->getUrl('opentag_testmodule_admin/testitems/edit');
+        return $this->getUrl('/testitems/edit');
     }
 }
